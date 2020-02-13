ea93f0805c017d33d27714e58de759faafa8c6c7
fanlei <929850056@qq.com>
2020-02-13 11:32:57 +0800

    dev tijiao

--- h5/src/router/module/index.js
+++
@@ -1,6 +0,0 @@
-import user from "./user";
-import order from "./order";
-import activity from "./activity";
-import admin from "./admin";
-
-export default [...user, ...order, ...activity, ...admin];
--- h5/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  footer: state => state.app.footer,
-  homeActive: state => state.app.homeActive,
-  home: state => state.app.home,
-  token: state => state.app.token,
-  isLogin: state => !!state.app.token,
-  backgroundColor: state => state.app.backgroundColor,
-  userInfo: state => state.app.userInfo || {}
-};
